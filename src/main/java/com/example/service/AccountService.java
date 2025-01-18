5dc1a0a860075e4570988e06870ca3d7936944d2
Andrei Miagkii <andreimiagkii@gmail.com>
2025-01-18 23:46:00 +0000

    Added method create account

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,34 @@
 package com.example.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import java.util.Optional;
+
+@Service
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account create(Account account) {
+
+        if(!accountRepository.findByUserName(account.getUsername()).isEmpty()) {    // Check duplicate account
+            return new Account("","");
+        } else if(!account.getUsername().isBlank() && account.getPassword().length() > 3) {     // Check account creating rules
+            Account createdAccount = new Account(account.getUsername(), account.getPassword());
+            return createdAccount;
+        } else return null;
+    }
+
+    public Optional<Account> login(Account account) {
+        return accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword());
+    }
+
 }
